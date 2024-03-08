a49d3fe15f6e42f1aac5c67a0394da7595b83a26
kajalthakur99 <kajal.01@dbalounge.com>
2024-03-08 17:21:12 +0530

    adding port

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
 const nextConfig = {
     reactStrictMode: true,
     env: {
-         BASE_URL: "http://10.12.12.46:80",
+         BASE_URL: "http://10.12.12.46:8090",
         // BASE_URL: "http://localhost:8080",
         GOOGLE_MAP_KEY: "AIzaSyCHM5WiE4qha0N3PXSOACt4_eaOBhS1Ts8"
     },
