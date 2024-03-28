dd88300a6e36f09fb5b2d9441373070f2a4f5dfb
kajal@88 <kajal.01@dbalounge.com>
2024-03-28 12:28:50 +0530

    upload date remove

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@
 const nextConfig = {
     reactStrictMode: true,
     env: {
-        //  BASE_URL: "http://10.12.12.46:8090",
-         BASE_URL: "http://192.168.5.208:8080",
+         BASE_URL: "http://10.12.12.46:8090",
+        //  BASE_URL: "http://192.168.5.208:8080",
         // BASE_URL: "http://localhost:8080",
         GOOGLE_MAP_KEY: "AIzaSyD0jvQz8BHejpyjquHdU3rrjLGSgRtPOh8"
     },
