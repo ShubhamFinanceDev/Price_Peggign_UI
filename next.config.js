69797f95487a357b33540cd1d285e8aaf6b4f66d
kajal@88 <kajal.01@dbalounge.com>
2024-04-03 14:16:41 +0530

    key set

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
          BASE_URL: "http://10.12.12.46:8090",
         //  BASE_URL: "http://192.168.5.208:8080",
         // BASE_URL: "http://localhost:8080",
-        GOOGLE_MAP_KEY: "AIzaSyD0jvQz8BHejpyjquHdU3rrjLGSgRtPOh8"
+        GOOGLE_MAP_KEY: "AIzaSyC1-MUz0bX_Bp_CXU97mm4Nmyf-Hj95rYw"
     },
 }
 
