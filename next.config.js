acf68393a8dd66c4ba67bffec765766cf120ce21
kajal@88 <kajal.01@dbalounge.com>
2024-04-11 00:40:33 +0530

    base url correct

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@
 const nextConfig = {
     reactStrictMode: true,
     env: {
-        //  BASE_URL: "http://10.12.12.46:8090",
-         BASE_URL: "http://192.168.5.208:8090",
+         BASE_URL: "http://10.12.12.46:8090",
+        //  BASE_URL: "http://192.168.5.208:8090",
         // BASE_URL: "http://localhost:8080",
         GOOGLE_MAP_KEY: "AIzaSyC1-MUz0bX_Bp_CXU97mm4Nmyf-Hj95rYw"
     },
