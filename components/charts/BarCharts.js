1889688ffd38f9399781d29ea983d1dc97ddd290
kajalthakur99 <kajal.01@dbalounge.com>
2024-05-24 11:45:46 +0530

    bar chart changes

--- components/charts/BarCharts.js
+++ components/charts/BarCharts.js
@@ -18,7 +18,7 @@ const BarCharts = ({ data = [], title = "" }) => {
                             format: 0
                         },
                         bar: {
-                            groupWidth: "20%" // Adjust this value as needed to change the bar width
+                            groupWidth: "10%" // Adjust this value as needed to change the bar width
                         }
                     }}
                 />
